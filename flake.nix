ee43c3b9eb3d9e7737f683f2d5e20e5be4fc9638
satler <satler@satler.dev>
2025-01-27 21:32:57 +0900

    feat: init flake

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs";
+  };
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          wrangler
+
+          pnpm
+          pnpm-shell-completion
+
+          nodejs_22
+        ];
+      };
+    };
+}
